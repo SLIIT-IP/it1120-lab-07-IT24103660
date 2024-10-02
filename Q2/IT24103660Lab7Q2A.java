41a5a0f90f79b8a91e5d1bb17c735e3d508dfd8f
it24103660 <it24103660@my.sliit.lk>
2024-10-02 06:46:24 +0530

    Add files via upload

---
+++ Q2/IT24103660Lab7Q2A.java
@@ -0,0 +1,16 @@
+public class IT24103660Lab7Q2A{
+
+    public static void main(String[] args) {
+  
+	//using for loop 	
+       for(int i = 1; i <= 4; i++)
+        {
+			for(int j = 1; j <= 5; j++)
+			{
+            //print the output
+				System.out.print("$");
+			}
+			System.out.println();
+        }
+    }
+}
